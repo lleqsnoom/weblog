d4b1a602c6592a2eece2c6b8144d83a6875a0f74
zasmarkany <lleqsnoom@gmail.com>
2014-07-13 12:54:46 +0200

    Beter stability under mobile devices

--- src/Main.hx
+++ src/Main.hx
@@ -3,6 +3,7 @@ package ;
 import pl.bigsoda.weblog.controllers.DebugController;
 import pl.bigsoda.weblog.controllers.LogController;
 import pl.bigsoda.weblog.controllers.TabNavigatorController;
+import pl.bigsoda.weblog.controllers.ServerAdressController;
 import pl.bigsoda.weblog.controllers.TestController;
 import pl.bigsoda.weblog.servicess.SocketService;
 import hxangular.Angular;
@@ -24,7 +25,9 @@ class Main
 		app.controller("pl.bigsoda.weblog.controllers.DebugController", DebugController);
 		app.controller("pl.bigsoda.weblog.controllers.TestController", TestController);
 		app.controller("pl.bigsoda.weblog.controllers.TabNavigatorController", TabNavigatorController);
+		app.controller("pl.bigsoda.weblog.controllers.ServerAdressController", ServerAdressController);
 		app.service("pl.bigsoda.weblog.servicess.SocketService", SocketService);
+
 	}
 	
 }
