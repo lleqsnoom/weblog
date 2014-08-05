e8a4dea1388214f18667f640383fcc806fe9b4ab
Tomasz Kwiatek <lleqsnoom@gmail.com>
2014-08-05 11:16:56 +0200

    added commands panel

--- WeblogRCE.hx
+++ WeblogRCE.hx
@@ -28,9 +28,16 @@ class WeblogRCE
 		var a:Array<Dynamic> = new Array<Dynamic>();
 
 		for(key in commands.keys()){
-			a.push({name: key, type: "function"});
+			a.push({name: key, type: commands.get(key).type});
 		}
-
+		a.sort(function(a:Dynamic, b:Dynamic):Int
+			{
+			    a = a.type;
+			    b = b.type;
+			    if (a < b) return -1;
+			    if (a > b) return 1;
+			    return 0;
+			});
 		return a;
 
 	}
@@ -43,7 +50,15 @@ class WeblogRCE
 
 
 	public function map(alias:String, element:Dynamic):Void {
-		commands.set(alias, element);
+		if(Std.is(element, Class)){
+			commands.set(alias, {element: element, type: "c"});
+		}else if(Reflect.isFunction(element)){
+			commands.set(alias, {element: element, type: "f"});
+		}else{
+			commands.set(alias, {element: element, type: "o"});
+		}
+
+		
 	}
 
 	#if hscript
@@ -63,7 +78,7 @@ class WeblogRCE
 
 				for(key in commands.keys()){
 					//Weblog.log(key);
-	 				interp.variables.set(key, commands.get(key));
+	 				interp.variables.set(key, commands.get(key).element);
 				}
 
 				Weblog.output(interp.execute(ast));
